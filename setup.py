41c970475187721097a68e7b0f604de24b81149e
gopalasr1968 <goplasr@gmail.com>
2025-08-23 22:11:32 +0530

    Folder structures added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical-chatbot",
+    version="0.0.0",
+    author="Srinivasan G",
+    author_email="gopalasr@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
